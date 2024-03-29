ccc7fc9523b608b092a4e15893cf379d0e8ebc5b
chenghl666 666 <15950044740@139.com>
2024-03-29 03:06:02 +0000

    initial

---
+++ src/main/java/org/单例模式/SingletonEHan.java
@@ -0,0 +1,38 @@
+package org.单例模式;
+
+public class SingletonEHan {
+
+    private SingletonEHan() {}
+
+    /**
+     * 1.单例模式的饿汉式[可用]
+     */
+    private static SingletonEHan singletonEHan = new SingletonEHan();
+
+    public static SingletonEHan getInstance() {
+        return singletonEHan;
+    }
+
+//     SingletonEHan instance= SingletonEHan.getInstance();
+
+    /**
+     * 2. 单例模式的饿汉式变换写法[可用]
+     * 基本没区别
+     */
+    private static SingletonEHan singletonEHanTwo = null;
+
+    static {
+        singletonEHanTwo = new SingletonEHan();
+    }
+
+    public static SingletonEHan getSingletonEHan() {
+        if (singletonEHanTwo == null) {
+            singletonEHanTwo = new SingletonEHan();
+        }
+        return singletonEHanTwo;
+    }
+    //     SingletonEHan instance= SingletonEHan.getSingletonEHan();
+
+
+}
+
